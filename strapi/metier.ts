f7424466a96e753db1bcaaf59d0e659dcff14b4d
Paul Bernard <bernardp.imag@gmail.com>
2025-11-20 14:52:43 +0100

    I've renamed those fields in strapi

--- strapi/metier.ts
+++ strapi/metier.ts
@@ -1,8 +1,7 @@
-import axiosClient from "@/services/axios";
-import { BlocksRenderer, type BlocksContent } from '@strapi/blocks-react-renderer';
-
+import axiosClient from "@/services/axios"
+import { BlocksRenderer, type BlocksContent } from "@strapi/blocks-react-renderer"
 
 export const getMetiers = async () => {
-  const response = await axiosClient.get<{data: {Titre: string, Description: BlocksContent}[]}>("metiers");
-  return response.data.data;
-};
+  const response = await axiosClient.get<{ data: { titre: string; description: BlocksContent }[] }>("metiers")
+  return response.data.data
+}
