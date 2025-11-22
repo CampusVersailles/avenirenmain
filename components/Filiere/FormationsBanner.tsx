81ee87885bf8b18254f1abad2c6ae6634bce1cd3
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-11-22 15:46:44 +0100

    fix: better home

--- components/Filiere/FormationsBanner.tsx
+++ components/Filiere/FormationsBanner.tsx
@@ -1,18 +1,22 @@
 import styles from "./FormationsBanner.module.css"
-import LinkAsButton from "../Button/LinkAsButton"
+import Tile from "../Tile/Tile"
+import ArrowRightIcon from "../Icons/ArrowRightIcon"
 
 const FormationsBanner = () => {
   return (
-    <div className={styles.banner}>
-      <h2>Découvre des métiers d'avenir</h2>
-      <p>
-        Que tu aimes les grands espaces ou les petits ateliers, travailler seul ou en groupe, bricoler ou animer,
-        esquisse ton futur en explorant les familles de métiers qui te plaisent.
-      </p>
-      <LinkAsButton secondary href='/formations' className={styles.button}>
-        Je trouve ma formation
-      </LinkAsButton>
-    </div>
+    <Tile href='/formations'>
+      <div className={styles.content}>
+        <h2 className={styles.title}>Découvre des métiers d'avenir</h2>
+        <p>
+          Que tu aimes les grands espaces ou les petits ateliers, travailler seul ou en groupe, bricoler ou animer,
+          esquisse ton futur en explorant les familles de métiers qui te plaisent.
+        </p>
+      </div>
+      <div className={styles.cta}>
+        <span>Je trouve ma formation</span>
+        <ArrowRightIcon />
+      </div>
+    </Tile>
   )
 }
 
