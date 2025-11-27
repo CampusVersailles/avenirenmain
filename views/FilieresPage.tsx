3c715fe94de33475bbb4712a2ebbc7758c66fd05
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-11-27 11:01:51 +0100

    feat: add breadcrumbs

--- views/FilieresPage.tsx
+++ views/FilieresPage.tsx
@@ -1,4 +1,5 @@
 import Block from "@/components/Block/Block"
+import Breadcrumbs from "@/components/Breadcrumbs/Breadcrumbs"
 import Filieres from "@/components/Filiere/Filieres"
 import FormationsBanner from "@/components/Filiere/FormationsBanner"
 import { Filiere } from "@/strapi/filieres"
@@ -6,6 +7,12 @@ import { Filiere } from "@/strapi/filieres"
 const FilieresPage = ({ filieres }: { filieres: Filiere[] }) => {
   return (
     <>
+      <Breadcrumbs
+        items={[
+          { label: "Accueil", href: "/" },
+          { label: "Filières", href: "/filieres" },
+        ]}
+      />
       <Block>
         <Filieres filieres={filieres} />
       </Block>
--- views/FormationsPage.tsx
+++ views/FormationsPage.tsx
@@ -1,13 +1,22 @@
 import Block from "@/components/Block/Block"
+import Breadcrumbs from "@/components/Breadcrumbs/Breadcrumbs"
 import Formations from "@/components/Formation/Formations"
 import { type Option } from "@/strapi/formations"
 
 const FormationsPage = ({ filieres, niveaux, durees }: { filieres: Option[]; niveaux: Option[]; durees: Option[] }) => {
   return (
-    <Block>
-      <h1>Les formations</h1>
-      <Formations filieres={filieres} niveaux={niveaux} durees={durees} />
-    </Block>
+    <>
+      <Breadcrumbs
+        items={[
+          { label: "Accueil", href: "/" },
+          { label: "Formations", href: "/formations" },
+        ]}
+      />
+      <Block>
+        <h1>Les formations</h1>
+        <Formations filieres={filieres} niveaux={niveaux} durees={durees} />
+      </Block>
+    </>
   )
 }
 
