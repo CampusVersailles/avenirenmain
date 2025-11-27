558075432c753d18be68e88fff5ba291ebd8e4c7
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-11-27 09:20:43 +0100

    fix: count on home page (#22)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+import { countFormations } from "@/strapi/formations"
+import { countMetiers } from "@/strapi/metier"
 import HomePage from "@/views/HomePage"
 
 export default async function Home() {
-  return <HomePage />
+  const [formationsCount, metiersCount] = await Promise.all([countFormations(), countMetiers()])
+  return <HomePage formationsCount={formationsCount} metiersCount={metiersCount} />
 }
--- views/HomePage.tsx
+++ views/HomePage.tsx
@@ -2,14 +2,14 @@ import Block from "@/components/Block/Block"
 import Banner from "@/components/Home/Banner"
 import Tiles from "@/components/Home/Tiles"
 
-const HomePage = () => {
+const HomePage = ({ formationsCount, metiersCount }: { formationsCount: number; metiersCount: number }) => {
   return (
     <>
       <Block>
         <Banner />
       </Block>
       <Block>
-        <Tiles />
+        <Tiles formationsCount={formationsCount} metiersCount={metiersCount} />
       </Block>
     </>
   )
