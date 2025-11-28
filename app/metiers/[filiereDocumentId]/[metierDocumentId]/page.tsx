0f76164200150ad4a44063612ed7720c911623c7
Paul Bernard <bernardp.imag@gmail.com>
2025-11-28 21:37:15 +0100

    Add generateStaticParams for dynamic routes.

--- app/metiers/[filiereDocumentId]/[metierDocumentId]/page.tsx
+++ app/metiers/[filiereDocumentId]/[metierDocumentId]/page.tsx
@@ -1,7 +1,23 @@
-import { getFiliereById } from "@/strapi/filieres"
+import { getFiliereById, getFilieres } from "@/strapi/filieres"
 import { getFormationsByRomeCode } from "@/strapi/formations"
 import { getMetier } from "@/strapi/metiers"
 import MetierPage from "@/views/MetierPage"
+
+export const dynamicParams = false
+
+export async function generateStaticParams() {
+  const filieres = await getFilieres()
+  const filieresWithMetiers = await Promise.all(filieres.map((filiere) => getFiliereById(filiere.documentId)))
+
+  const results = filieresWithMetiers.flatMap((filiere) =>
+    filiere.metiers.map((metier) => ({
+      filiereDocumentId: filiere.documentId,
+      metierDocumentId: metier.documentId,
+    })),
+  )
+
+  return results
+}
 
 export default async function FiliereMetiers({
   params,
--- app/metiers/[filiereDocumentId]/page.tsx
+++ app/metiers/[filiereDocumentId]/page.tsx
@@ -1,5 +1,15 @@
-import { getFiliereById } from "@/strapi/filieres"
+import { getFiliereById, getFilieres } from "@/strapi/filieres"
 import FiliereMetiersPage from "@/views/FiliereMetiersPage"
+
+export const dynamicParams = false
+
+export async function generateStaticParams() {
+  const filieres = await getFilieres()
+
+  return filieres.map((filiere) => ({
+    filiereDocumentId: filiere.documentId,
+  }))
+}
 
 export default async function FiliereMetiers({ params }: { params: Promise<{ filiereDocumentId: string }> }) {
   const { filiereDocumentId } = await params
