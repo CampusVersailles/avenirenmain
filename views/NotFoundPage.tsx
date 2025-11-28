6a009e2ec37e3078d8b3711b2fd624db570e83bf
Paul Bernard <bernardp.imag@gmail.com>
2025-11-28 16:51:35 +0100

    Add not-found page.

---
+++ views/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+import Block from "@/components/Block/Block"
+import NotFoundBanner from "@/components/Errors/NotFoundBanner"
+
+export default function NotFoundPage() {
+  return (
+    <Block>
+      <NotFoundBanner />
+    </Block>
+  )
+}
