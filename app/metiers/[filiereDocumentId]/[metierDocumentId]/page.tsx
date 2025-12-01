3fbe583e2bcf32b6c504bd11efc8fb91162f554a
Paul Bernard <bernardp.imag@gmail.com>
2025-12-01 12:26:53 +0100

    Remove generateStaticParams

--- app/metiers/[filiereDocumentId]/[metierDocumentId]/page.tsx
+++ app/metiers/[filiereDocumentId]/[metierDocumentId]/page.tsx
@@ -1,20 +1,7 @@
-import { getDomainesPro, getFiliereById, getFilieresAndMetiersDocumentIdsOnly } from "@/strapi/filieres"
+import { getDomainesPro, getFiliereById } from "@/strapi/filieres"
 import { getMetier } from "@/strapi/metiers"
 import MetierPage from "@/views/MetierPage"
 import { notFound } from "next/navigation"
-
-export async function generateStaticParams() {
-  const filieresWithMetiers = await getFilieresAndMetiersDocumentIdsOnly()
-
-  const results = filieresWithMetiers.flatMap((filiere) =>
-    filiere.metiers.map((metier) => ({
-      filiereDocumentId: filiere.documentId,
-      metierDocumentId: metier.documentId,
-    })),
-  )
-
-  return results
-}
 
 export default async function FiliereMetiers({
   params,
@@ -23,8 +10,8 @@ export default async function FiliereMetiers({
 }) {
   const { filiereDocumentId, metierDocumentId } = await params
   const [filiere, metier, domainesPro] = await Promise.all([
-    getFiliereById(filiereDocumentId),
-    getMetier(metierDocumentId),
+    getFiliereById(filiereDocumentId).catch(() => null),
+    getMetier(metierDocumentId).catch(() => null),
     getDomainesPro(),
   ])
 
--- app/metiers/[filiereDocumentId]/page.tsx
+++ app/metiers/[filiereDocumentId]/page.tsx
@@ -1,18 +1,13 @@
-import { getDomainesPro, getFiliereById, getFilieres } from "@/strapi/filieres"
+import { getDomainesPro, getFiliereById } from "@/strapi/filieres"
 import FiliereMetiersPage from "@/views/FiliereMetiersPage"
 import { notFound } from "next/navigation"
 
-export async function generateStaticParams() {
-  const filieres = await getFilieres()
-
-  return filieres.map((filiere) => ({
-    filiereDocumentId: filiere.documentId,
-  }))
-}
-
 export default async function FiliereMetiers({ params }: { params: Promise<{ filiereDocumentId: string }> }) {
   const { filiereDocumentId } = await params
-  const [filiere, domainesPro] = await Promise.all([getFiliereById(filiereDocumentId), getDomainesPro()])
+  const [filiere, domainesPro] = await Promise.all([
+    getFiliereById(filiereDocumentId).catch(() => null),
+    getDomainesPro(),
+  ])
 
   if (!filiere) {
     notFound()
