e88b9b330485d914dde57458170bbb29d8bb3615
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-12-02 12:08:42 +0100

    feat: display metiers proche

--- components/Metier/Metier.tsx
+++ components/Metier/Metier.tsx
@@ -9,7 +9,6 @@ import MetierPerspectives from "./MetierSectionPerspectives"
 import MetiersTabs, { TabItem } from "./Tabs/MetiersTabs"
 import TabSpecialization from "./Tabs/TabAppellations"
 import TabSalaire from "./Tabs/TabSalaire"
-import TabMetiersProches from "./Tabs/TabMetiersProches"
 import TabFormations from "./Tabs/TabFormations"
 
 const Metier = ({
@@ -30,12 +29,28 @@ const Metier = ({
     {
       id: "specialisations",
       label: "Spécialisations",
-      component: <TabSpecialization filiere={filiere} metier={metier} />,
+      component: (
+        <TabSpecialization
+          filiere={filiere}
+          appellations={metier.appellations}
+          notFoundMessage='Aucune spécialisation disponible pour ce métier.'
+        />
+      ),
     },
-    { id: "metiers", label: "Métiers proches", component: <TabMetiersProches metier={metier} /> },
+    {
+      id: "metiers",
+      label: "Métiers proches",
+      component: (
+        <TabSpecialization
+          filiere={filiere}
+          appellations={metier.metiersProches}
+          notFoundMessage='Aucun métier proche disponible pour ce métier.'
+        />
+      ),
+    },
     { id: "salaire", label: "Salaire estimé", component: <TabSalaire metier={metier} /> },
   ]
-
+  console.log(metier.metiersProches)
   return (
     <div className={styles.metier}>
       <MetierBanner metier={metier} domainesPro={domainesPro} />
