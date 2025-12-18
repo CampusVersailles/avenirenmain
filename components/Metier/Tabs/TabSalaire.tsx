496a67c2a7acc206d82f54292d954ac71d093c16
Paul Bernard <bernardp.imag@gmail.com>
2025-12-18 21:56:40 +0100

    Improve salary display (rounding, no display if empty, wording)

--- components/Metier/Tabs/TabSalaire.tsx
+++ components/Metier/Tabs/TabSalaire.tsx
@@ -2,6 +2,10 @@ import SalaryIcon from "@/components/Icons/Salary"
 import styles from "./TabSalaire.module.css"
 import { Metier as MetierType } from "@/strapi/metiers"
 
+function roundToTen(value: number) {
+  return Math.round(value / 10) * 10
+}
+
 export default function TabSalaire({ metier }: { metier: MetierType }) {
   const salaireDisponible = metier.salaire && metier.salaire.valeur_basse && metier.salaire.valeur_haute
   return (
@@ -10,8 +14,8 @@ export default function TabSalaire({ metier }: { metier: MetierType }) {
       <div className={styles.verticalSeparator} />
       {salaireDisponible ? (
         <p className={styles.text}>
-          Après plusieurs années d'expérience, vous pouvez espérer un salaire compris entre{" "}
-          {metier.salaire.valeur_basse} € et {metier.salaire.valeur_haute} €.
+          Le salaire moyen brut mensuel est compris entre {roundToTen(metier.salaire.valeur_basse)} € et{" "}
+          {roundToTen(metier.salaire.valeur_haute)} € (source : France Travail)
         </p>
       ) : (
         <p className={styles.text}>Salaire non renseigné pour ce métier.</p>
