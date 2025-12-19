5190579525968aa46dc5618703c5ff6284d85e21
Paul Bernard <bernardp.imag@gmail.com>
2025-12-19 09:01:46 +0100

    review follow-up (salaire always available in tab)

--- components/Metier/Tabs/TabSalaire.tsx
+++ components/Metier/Tabs/TabSalaire.tsx
@@ -7,19 +7,14 @@ function roundToTen(value: number) {
 }
 
 export default function TabSalaire({ metier }: { metier: MetierType }) {
-  const salaireDisponible = metier.salaire && metier.salaire.valeur_basse && metier.salaire.valeur_haute
   return (
     <div className={styles.container}>
       <SalaryIcon className={styles.icon} />
       <div className={styles.verticalSeparator} />
-      {salaireDisponible ? (
-        <p className={styles.text}>
-          Le salaire moyen brut mensuel est compris entre {roundToTen(metier.salaire.valeur_basse)} € et{" "}
-          {roundToTen(metier.salaire.valeur_haute)} € (source : France Travail)
-        </p>
-      ) : (
-        <p className={styles.text}>Salaire non renseigné pour ce métier.</p>
-      )}
+      <p className={styles.text}>
+        Le salaire moyen brut mensuel est compris entre {roundToTen(metier.salaire.valeur_basse)} € et{" "}
+        {roundToTen(metier.salaire.valeur_haute)} € (source : France Travail)
+      </p>
     </div>
   )
 }
