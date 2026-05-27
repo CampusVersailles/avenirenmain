4128aac218f3de71e88c48b9095dbf3fb3230a42
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2026-05-27 14:26:18 +0200

    fix: missing image

--- lib/media_utils.ts
+++ lib/media_utils.ts
@@ -1,4 +1,8 @@
 export function getMediaUrl(media: { provider?: string; url: string }) {
+  if (!media) {
+    return ""
+  }
+
   if (media.provider === "local") {
     return `${process.env.STRAPI_URL}${media.url}`
   }
