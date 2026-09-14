12cd14dba52b0268ee505b971bf92be387774023
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2026-09-14 20:58:31 +0100

    feat: add type and echelle on dispositifs

--- components/Ressources/FinancementBanner.tsx
+++ components/Ressources/FinancementBanner.tsx
@@ -1,7 +1,7 @@
-import { MetaFichePratiqueStrapi } from "@/strapi/ressources";
-import styles from "./GuideBanner.module.css";
-import StrapiRichText from "../Strapi/StrapiRichText";
-import Image from "next/image";
+import { MetaFichePratiqueStrapi } from "@/strapi/ressources"
+import styles from "./GuideBanner.module.css"
+import StrapiRichText from "../Strapi/StrapiRichText"
+import Image from "next/image"
 
 const FinancementBanner = ({ meta }: { meta: MetaFichePratiqueStrapi }) => {
   return (
@@ -11,7 +11,7 @@ const FinancementBanner = ({ meta }: { meta: MetaFichePratiqueStrapi }) => {
         <StrapiRichText content={meta.description} className={styles.description} />
       </div>
       <>
-        <Image className={styles.image} src='/images/guide.png' alt='' width={1140} height={519} />
+        <Image className={styles.image} src='/images/cameo.png' alt='' width={1140} height={519} />
       </>
     </>
   )
